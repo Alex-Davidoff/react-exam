b7073a08b5d78749be2272c0cb60819bf9df0258
Alex-Davidoff <alex.davidoff0009@gmail.com>
2025-01-29 14:46:35 +0200

    e 2

---
+++ src/routes/router.tsx
@@ -0,0 +1,19 @@
+import { Navigate, createBrowserRouter } from "react-router";
+import { MainLayout } from "../layouts/MainLayout";
+import { MainPage } from "../pages/MainPage";
+import { LoginPage } from "../pages/LoginPage";
+import { UsersPage } from "../pages/UsersPage";
+import { RecipesPage } from "../pages/RecipesPage";
+import { AuthLayout } from "../layouts/AuthLayout";
+
+export const routes = createBrowserRouter([
+    {path: '/', element: <MainLayout/>, children: [
+        {index:true, element: <Navigate to='main' replace />},
+        {path: 'main', element: <MainPage/>},
+        {path: 'login', element: <LoginPage/>},
+        {path: 'auth', element: <AuthLayout/>, children: [
+            {path: 'users', element: <UsersPage/>},
+            {path: 'recipes', element: <RecipesPage/>}
+        ]}
+    ]}
+]);
