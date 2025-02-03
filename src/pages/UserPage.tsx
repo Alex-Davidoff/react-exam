70b5fce764a7c34fd4372db2aa29ff9f66fff0d2
Alex-Davidoff <alex.davidoff0009@gmail.com>
2025-02-03 02:31:13 +0200

    e 24

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -1,12 +1,27 @@
-import { useParams } from "react-router";
+import { useParams, useSearchParams } from "react-router";
 import { UserComponent } from "../components/UsersComponents/UserComponent";
-import { useMainSelector } from "../redux/store";
+import { useMainDispatch, useMainSelector } from "../redux/store";
+import { useEffect } from "react";
+import { userActions } from "../redux/slices/userSlice";
 
 export const UserPage = () => {
     const {id} = useParams();
 
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const dispatch = useMainDispatch();
     const users = useMainSelector((state) => state.userStoreSlice.users);
 
+    useEffect(() => {
+        if (!users) {
+            if (id) {
+                searchParams.set('q', id);
+                setSearchParams(searchParams);
+                dispatch(userActions.loadUsers(searchParams));
+            } 
+        }
+    },[searchParams]);
+    
     if (users){
     return(
         <div>
